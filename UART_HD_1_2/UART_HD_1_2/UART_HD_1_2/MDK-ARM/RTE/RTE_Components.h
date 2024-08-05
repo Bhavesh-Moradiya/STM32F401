7286329444068afbbfd7d43558162b88ad64ce84
Bhavesh M Moradiya <bhavesh.moradi@gmail.com>
2024-08-05 15:48:14 +0530

    USART

---
+++ UART_HD_1_2/UART_HD_1_2/UART_HD_1_2/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'UART_HD_1_2' 
+ * Target:  'UART_HD_1_2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */

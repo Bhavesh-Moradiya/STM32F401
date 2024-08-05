c9bab418690bed90ce4f30833653fb0e2ddc7c2a
Bhavesh M Moradiya <bhavesh.moradi@gmail.com>
2024-08-05 15:46:43 +0530

    Basic GPIO Test

---
+++ GPIO/GPIO/GPIO_OUTPUT/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GPIO_OUTPUT' 
+ * Target:  'GPIO_OUTPUT' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
